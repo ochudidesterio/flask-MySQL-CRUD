2975f499886012822e869df9b87b1508bd609db1
DESTERIO-RASMENTO <desochudi@gmail.com>
2020-05-11 19:57:47 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template,redirect,request,url_for
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']=''
+app.config['MYSQL_DB']='flaskcrud'
+
+mysql = MySQL(app)
+
+@app.route('/')
+
+def index():
+    return render_template ('base.html')
+
+@app.route('/insert', methods=['POST'])
+def insert():
+    if request.method=="POST":
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO student (name,email,phone) values(%s,%s,%s)",(name,email,phone))
+        mysql.connection.commit()
+        return redirect(url_for('index'))
+
+if __name__=="__main__":
+    app.run(debug=True)
